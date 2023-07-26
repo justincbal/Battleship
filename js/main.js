ecc10a4c6ea2fe3c48f3a4059ebfdad57fe88bf4
Justin Balagtas <justinseebee@hotmail.com>
2023-07-26 14:29:20 -0400

    add playerboards ui

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 // Apply grid items
 const board = document.querySelectorAll(".board");
 
-for (let i = 0; i < board.length; i++) {
-  for (let j = 0; j < 100; j++) {
-    const div = document.createElement("div");
-    div.classList.add("cell");
-    board[i].append(div);
+board.forEach((bd) => {
+  for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < 10; j++) {
+      const div = document.createElement('div');
+      const coord = `${i}-${j}`;
+      div.classList.add('cell',`${coord}`);
+      div.textContent = coord;
+      bd.append(div);
+    }
+    
   }
-}
+})
+
+
+// Menu buttons
+const menuButton = document.querySelectorAll(".menuOption");
+const menu = document.querySelector(".menu");
+const main = document.querySelector('.main');
+menuButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("CLICK");
+    menu.style.display = 'none';
+    main.classList.toggle('blur');
+  });
+});
